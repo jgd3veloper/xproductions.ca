00403985a205990ae58409b3f4cb0ad0ab3d744d
Joseph Giancola <jgd3veloper@gmail.com>
2021-02-24 18:08:32 -0500

    Converted Static Website into a PWA

---
+++ sw.js
@@ -0,0 +1,50 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  var CACHE_NAME = 'my-site-cache-v1';
+  var urlsToCache = [
+    '/',
+    'assets/js/jquery.min.js',
+    'assets/js/browser.min.js',
+    'assets/js/breakpoints.min.js',
+    'assets/js/main.js',
+    'assets/css/main.css',
+    'assets/css/noscript.css'
+  ];
+
+  self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+          console.log('Opened cache');
+          return cache.addAll(urlsToCache);
+        })
+    );
+});
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
